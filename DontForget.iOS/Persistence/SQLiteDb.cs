4a3cd0254a141873317d32f7aba6e20f6793de62
chrisgilliam72 <54978118+chrisgilliam72@users.noreply.github.com>
2019-11-08 19:09:48 +0200

    Add files via upload

---
+++ DontForget.iOS/Persistence/SQLiteDb.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using DontForget.iOS;
+using SQLite;
+using Xamarin.Forms;
+using DontForget.Persistence;
+
+[assembly: Dependency(typeof(SQLiteDB))]
+
+namespace DontForget.iOS
+{
+    public class SQLiteDB : ISQLiteDB
+    {
+
+        public SQLiteAsyncConnection GetConnection()
+        {
+            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var libpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "..", "Library");
+            var path = Path.Combine(libpath, "MySQLite.db3");
+
+            return new SQLiteAsyncConnection(path);
+        }
+    }
+}
